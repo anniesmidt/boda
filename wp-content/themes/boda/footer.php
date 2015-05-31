761053adf0c3871219064b3c69b404399a6e2fc3
Annie Smidt <annie@durablecreative.com>
2015-05-31 18:27:31 -0400

    initial changes to files/setup

---
+++ wp-content/themes/boda/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package boda
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'boda' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'boda' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'boda' ), 'boda', '<a href="http://www.durablecreative.com" rel="designer">Annie Smidt at Durable Creative</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/boda/template-parts/content-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package boda
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+    <!-- display page titles, unless it's the front home page -->
+  	<?php if (is_front_page() ) { /*Do nothing*/ } else { the_title(); } ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'boda' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php edit_post_link( esc_html__( 'Edit', 'boda' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
